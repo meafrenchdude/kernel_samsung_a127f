b58c1db5c94b2326151c8321a6fa9a0a7d367b29
A French Dude <59694243+meafrenchdude@users.noreply.github.com>
2026-07-13 23:45:38 +0200

    fix #3

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -21,7 +21,7 @@
 #include "pnode.h"
 #include "internal.h"
 
-static __poll_t mounts_poll(struct file *file, poll_table *wait)
+static __poll_t mounts_poll(struct file *file, poll_table *wait);
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 extern bool susfs_hide_sus_mnts_for_non_su_procs;
 extern bool susfs_is_current_ksu_domain(void);
